2b075aa4e965cadb0967e03e22477f53c50a5636
shl422 <h.k.domingo@outlook.com>
2021-08-02 13:41:29 +0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return 'This one is on me!'
+  }
+  else if (distance >2000 && distance <2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (distance > 2500){
+    return 'No can do.'
+  }
 }
+// Write your code here!
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ if (city === 'NYC'){
+   return 'Ok, sounds good.'
+ } else {
+   return 'No go.'
+  }
 }
+ // Write your code here!
+
+
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous'){
+    return 'Thank you so much.'
+   } else if (tip === 'not as generous'){
+     return 'Thank you.'
+   }else if (tip === 'thanks for everything'){
+    return 'Bye.'
+   }  
+  }
+
+  
+  // Write your code here!
